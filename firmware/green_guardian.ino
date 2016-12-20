e8324ba8c62bd87af70ab186af3b712ca80d526f
Thomas Steinholz <tsteinholz@yahoo.com>
2016-12-19 19:36:13 -0500

    formatted arduino project

---
+++ firmware/green_guardian.ino
@@ -0,0 +1,79 @@
+#include <SymphonyLink.h>
+
+//Instantiate a SymphonyLink class using the SymphonyLink Arduino Library
+SymphonyLink symlink; 
+
+uint8_t txData[256];    //TX data buffer. Could be up to 256 bytes.
+uint8_t rxData[128];   //RX data buffer. Could be up to 128 bytes.
+uint8_t rxDataLength;
+uint8_t radioPath = 1; // set 1 for U.FL, 2 for integrated trace antenna
+
+sym_module_state_t currentSymphonyState;
+sym_module_state_t lastSymphonyState;
+
+void setup() 
+{
+  int ii;
+  
+  //Arduino Due allows debug and status signals out of the Serial port. UART communications with the SymphonyLink module are done using Serial1.
+  Serial.begin(115200);
+
+  //Configure the following to match your network and application tokens
+  //Set desired network token 
+  uint32_t netToken = 0x0;  //Insert your network token. For example, the OPEN network token is 0x4f50454e.
+                                   //A module can only talk to gateways using the same network token.
+  //Set desired application token
+  uint8_t appToken[APP_TOKEN_LEN] = {0x0}; //Generate an application token in your Conductor account use it here.
+                                                                                          //The application token identifies this dataflow in Conductor.
+  
+  //Initialize the SymphonyLink object and open UART communications with the module on Serial1.
+  symlink.begin(netToken, appToken, LL_DL_MAILBOX, 15);
+
+  //Initialize the txData. This is the array of hex bytes to be sent over the air, as an example.
+  for (ii = 0; ii < sizeof(txData); ii++)
+  {
+    txData[ii] = 0;
+  }
+
+  rxDataLength = sizeof(rxData);
+
+  //Set RF path
+  symlink.setAntenna(radioPath);
+  
+  //Update the state of the SymphonyLink module (aka Modem)
+  lastSymphonyState = symlink.updateModemState();
+}
+
+void loop()
+{
+  //Update the state of the SymphonyLink module (aka Modem)
+  currentSymphonyState = symlink.updateModemState();
+  switch (currentSymphonyState)
+  {
+    case SYMPHONY_READY:                            
+      if (SYMPHONY_TRANSMITTING != lastSymphonyState) //When SymphonyLink module is ready, send txData
+      {
+        txData[0]++;                                 //Increment payload
+        symlink.write(txData, sizeof(txData), true); //Uplink the payload to Conductor
+      
+        //Print the payload to the Serial monitor for debug.
+        Serial.print("\t... Outbound payload is ");
+        symlink.printPayload(txData, sizeof(txData));
+      }
+      else 
+      {
+         // If last uplink failed, do not increment payload.
+         if (LL_TX_STATE_SUCCESS != symlink.getTransmitState())
+         {
+          txData[0]--;
+         }
+
+         //Check for downlink data
+         symlink.read(rxData, rxDataLength);
+      }
+      break;
+    default:
+      break;
+  }
+  lastSymphonyState = currentSymphonyState;
+}
